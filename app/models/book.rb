4de9beb9d09e98c093937dfb709ddd9b77da0205
M0EKA <momoseaoi.1810@gmail.com>
2021-10-11 16:21:24 +0900

    カスタムバリデーションをprivateメソッドで実装してみる

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,12 @@
 class Book < ApplicationRecord
+  validate :title_valid?
+  validates :title, length: { in: 4..32 }
+
+  private
+
+  def title_valid?
+    if self.title.match(/test/)
+      errors.add(:title, "testという文字の入ったtitleは登録できない")
+    end
+  end
 end
